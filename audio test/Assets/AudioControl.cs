bfe5d0f9b38908c2bd28a56fe0a2853b5e385a78
Brad Pepers <bpepers@me.com>
2020-02-01 10:43:10 -0700

    Set everything through  mixer

--- audio test/Assets/AudioControl.cs
+++ audio test/Assets/AudioControl.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class AudioControl : MonoBehaviour
 {
     public AudioSource audio1;
+    public AudioMixer audioMixer;
     public Slider pitchSlider;
     public Slider tempoSlider;
 
@@ -16,7 +18,17 @@ public class AudioControl : MonoBehaviour
 
     public void TempoChanged()
     {
-        audio1.pitch = 1.0f + tempoSlider.value / 100.0f;
-        // TODO: also need to adjust pitch down
+        float tempo;
+        if (tempoSlider.value >= 0.0f)
+        {
+            tempo = tempoSlider.value + 1.0f;
+        }
+        else
+        {
+            tempo = 1.0f / (-tempoSlider.value + 1.0f);
+        }
+
+        audioMixer.SetFloat("masterTempo", tempo);
+        audioMixer.SetFloat("masterPitch", 1.0f / tempo);
     }
 }
