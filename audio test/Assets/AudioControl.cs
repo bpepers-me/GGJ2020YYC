5288a5479361b25cbb574445a091f09c5b1bc0ae
Brad Pepers <bpepers@me.com>
2020-02-01 09:29:19 -0700

    Start of modifying audio pitch and tempo using UI

---
+++ audio test/Assets/AudioControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioControl : MonoBehaviour
+{
+    public AudioSource audio1;
+    public Slider pitchSlider;
+    public Slider tempoSlider;
+
+    public void PitchChanged()
+    {
+        // TODO: use audio mixer to set the pitch
+    }
+
+    public void TempoChanged()
+    {
+        audio1.pitch = 1.0f + tempoSlider.value / 100.0f;
+        // TODO: also need to adjust pitch down
+    }
+}
