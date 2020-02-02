eab97c7ed3782d434f65040dd36a40253d487fb0
Brad Pepers <bpepers@me.com>
2020-02-02 15:17:00 -0700

    Added reference audio playing

---
+++ audio test/Assets/AudioReferenceControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioReferenceControl : MonoBehaviour
+{
+    public AudioSource referenceSource;
+    public AudioSource[] trackSources;
+
+    public void PlayReferenceAudio()
+    {
+        foreach(var source in trackSources)
+        {
+            source.Stop();
+        }
+
+        float startTime = 5.0f;
+
+        referenceSource.time = startTime;
+        referenceSource.Play();
+
+        StartCoroutine(RestoreAudio(5f));
+    }
+
+    private IEnumerator RestoreAudio(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        referenceSource.Stop();
+
+        foreach (var source in trackSources)
+        {
+            source.Play();
+        }
+    }
+}
