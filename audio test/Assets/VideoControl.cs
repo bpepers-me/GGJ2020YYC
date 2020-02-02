3ed17d0284ccd012e516fc9ce9b7cc310866275f
Alexei Pepers <pikalex88@gmail.com>
2020-02-02 13:56:27 -0700

    Hook up sliders to video materials

---
+++ audio test/Assets/VideoControl.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class VideoControl : MonoBehaviour
+{
+    public Slider slider;
+    private Material videoMaterial;
+    public string materialParamName;
+    public bool isFixed;
+    public Renderer renderer;
+
+    void Start()
+    {
+        videoMaterial = renderer.material;
+        int halfLength = 40;
+        int minValue = -halfLength + 1;
+        int maxValue = halfLength - 1;
+        int magicNumber = Random.Range(minValue, maxValue);
+        slider.minValue = magicNumber - halfLength;
+        slider.maxValue = magicNumber + halfLength;
+        if (!isFixed) {
+            slider.value = slider.minValue + halfLength;
+            SliderUpdated();
+            slider.onValueChanged.AddListener (delegate {SliderUpdated ();});
+        } else {
+            slider.enabled = false;
+        }
+    }
+
+    public void SliderUpdated() {
+        float sliderValue = Mathf.Round(slider.value / 4.0f) * 4.0f;
+        sliderValue = Mathf.Abs(sliderValue);
+        sliderValue = sliderValue / 100.0f;
+
+        videoMaterial.SetFloat(materialParamName, sliderValue);
+    }
+}
