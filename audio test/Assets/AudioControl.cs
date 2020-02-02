e5ba521aa4db60c605391fb59b0f0487dff8631e
Alexei Pepers <pikalex88@gmail.com>
2020-02-01 18:15:32 -0700

    Hook up 3 pitch sliders with a random 'correct' value in range

--- audio test/Assets/AudioControl.cs
+++ audio test/Assets/AudioControl.cs
@@ -6,41 +6,69 @@ using UnityEngine.Audio;
 
 public class AudioControl : MonoBehaviour
 {
-    public AudioMixer audioMixer;
-    public Slider pitchSlider;
-    public Slider tempoSlider;
+    //public AudioMixer audioMixer;
+    public Slider slider;
+    public AudioSource audioSource;
+    
+    //public Slider pitchSlider;
+    //public Slider tempoSlider;
 
-    private float GetTempo()
+    void Start()
+    {
+        int halfLength = 40;
+        int minValue = -halfLength + 1;
+        int maxValue = halfLength - 1;
+        int magicNumber = Random.Range(minValue, maxValue);
+        slider.minValue = magicNumber - halfLength;
+        slider.maxValue = magicNumber + halfLength;
+        Debug.Log("Min = " + minValue + ", Max = " + maxValue + ", magic = " + magicNumber);
+        Debug.Log("Slider min = " + slider.minValue + ", slider max = " + slider.maxValue);
+        slider.onValueChanged.AddListener (delegate {SliderUpdated ();});
+    }
+
+    public void SliderUpdated() {
+        Debug.Log("Min = " + slider.minValue + ", Max = " + slider.maxValue);
+        Debug.Log(slider.value);
+
+        audioSource.pitch = SliderToPitch(slider.value);
+        Debug.Log(SliderToPitch(slider.value));
+    }
+
+    private float SliderToPitch(float sliderValue) {
+        return GetPitch(sliderValue * (3.0f/100.0f));
+    }
+
+    private float GetTempo(float tempoValue)
     {
         float tempo;
-        if (tempoSlider.value >= 0.0f)
+        if (tempoValue >= 0.0f)
         {
-            tempo = tempoSlider.value + 1.0f;
+            tempo = tempoValue + 1.0f;
         }
         else
         {
-            tempo = 1.0f / (-tempoSlider.value + 1.0f);
+            tempo = 1.0f / (-tempoValue + 1.0f);
         }
 
         return tempo;
     }
 
-    private float GetPitch()
+    private float GetPitch(float pitchValue)
     {
         float pitch;
-        if (pitchSlider.value >= 0.0f)
+        if (pitchValue >= 0.0f)
         {
-            pitch = pitchSlider.value + 1.0f;
+            pitch = pitchValue + 1.0f;
         }
         else
         {
-            pitch = 1.0f / (-pitchSlider.value + 1.0f);
+            pitch = 1.0f / (-pitchValue + 1.0f);
         }
 
-        return pitch / GetTempo();
+        return pitch; // /GetTempo()
     }
 
-    public void UpdateAudio()
+    /*public void UpdateAudio()
     {
         audioMixer.SetFloat("masterTempo", GetTempo());
         audioMixer.SetFloat("masterPitch", GetPitch());
@@ -54,5 +82,5 @@ public class AudioControl : MonoBehaviour
     public void TempoChanged()
     {
         UpdateAudio();
-    }
+    }*/
 }
