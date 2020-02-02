74fc30a212121c6bfbbcf5fd69fda575100ec6f6
Brad Pepers <bpepers@me.com>
2020-02-02 13:24:39 -0700

    Audio stop/restart/play buttons all work

---
+++ audio test/Assets/PlayControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayControl : MonoBehaviour
+{
+    public AudioSource source1;
+    public AudioSource source2;
+    public AudioSource source3;
+
+    public void RestartAudio()
+    {
+        StopAudio();
+        PlayAudio();
+    }
+
+    public void StopAudio()
+    {
+        source1.Stop();
+        source2.Stop();
+        source3.Stop();
+    }
+
+    public void PlayAudio()
+    {
+        source1.Play();
+        source2.Play();
+        source3.Play();
+    }
+}
