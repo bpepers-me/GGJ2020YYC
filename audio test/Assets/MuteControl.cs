c0a8b739df3401544a1a5b106b105acdff0eaa44
Brad Pepers <bpepers@me.com>
2020-02-02 11:39:46 -0700

    Better mute buttons

--- audio test/Assets/MuteControl.cs
+++ audio test/Assets/MuteControl.cs
@@ -1,13 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MuteControl : MonoBehaviour
 {
     public AudioSource audioSource;
+    public Image offImage;
 
     public void Clicked()
     {
         audioSource.mute = !audioSource.mute;
+        offImage.gameObject.SetActive(audioSource.mute);
     }
 }
